cee2288aca237f64158d49cd38d819ca84c1de51
Graham Wright <gwright99@hotmail.com>
2023-01-15 15:45:10 -0500

    Merge functional Drone promotion configuration (#3)
    Squashed hugely-noisy testing branch. This commit has active configuration that works. Commented items are a bit of a grab bag of stuff that works and other stuff that totally breaks.

---
+++ create_workspace.py
@@ -0,0 +1,107 @@
+import httpx
+import argparse
+import json
+import base64
+
+parser  = argparse.ArgumentParser(description='Test Nextflow Tower Label limit (default 100)')
+# parser.add_argument("--action", help="Specify `create` or `delete`")
+parser.add_argument("--token", help="Tower PAT token")
+parser.add_argument("--wsname", help="Name of workspace")
+parser.add_argument("--useremail", help="Email of user to add")
+parser.add_argument("--b64credential", help="The secret credential")
+
+# tower_token = "YOUR_TOWER_TOKEN"
+tower_base_url = "dc.seqera.grahamwright.net"
+tower_org_id = "182463563746108"
+
+
+def create_org_data(wsname: str, useremail: str):
+    '''Create the payload data to create a workspace.'''
+    create_org_data = {
+        "workspace": {
+            "name": wsname,
+            "fullName": f"Workspace for {useremail}.",
+            "visibility": "PRIVATE",
+            "description": None
+        }
+    }
+    return create_org_data
+
+
+if __name__ == '__main__':
+
+    '''
+    Fill this out later with usage details.
+    '''
+
+    args, unknown = parser.parse_known_args()
+    print(args.token)
+    print(args.wsname)
+    print(args.useremail)
+    print(args.b64credential)
+
+    headers={
+        "Authorization": f"Bearer {args.token}",
+        "Accept_Version": "1.0",
+        "Content_Type": "application/json"
+    }
+
+    # Add User to Org
+    url = f"https://{tower_base_url}/api/orgs/{tower_org_id}/members/add"
+    payload = { "user": args.useremail }
+    resp = httpx.put(url=url, json=payload, headers=headers)
+    print(resp)
+
+    # Create Workspace
+    url = f"https://{tower_base_url}/api/orgs/{tower_org_id}/workspaces"
+    payload = create_org_data(wsname=args.wsname, useremail=args.useremail)
+    resp = httpx.post(url=url, json=payload, headers=headers)
+    print(resp.content)
+    # Response comes back as b'{json}'. Convert to json to grab keys.
+    resp_json = json.loads(resp.content.decode('utf-8'))
+    workspace_id = resp_json['workspace']['id']
+    print(workspace_id)
+
+    # Add user to workspace
+    ## workspace_id="248571532740296"
+    url = f"https://{tower_base_url}/api/orgs/{tower_org_id}/workspaces/{workspace_id}/participants/add"
+    payload = { "userNameOrEmail": args.useremail }
+    resp = httpx.put(url=url, json=payload, headers=headers)
+    print(resp.content)
+    # Response comes back as b'{json}'. Convert to json to grab keys.
+    resp_json = json.loads(resp.content.decode('utf-8'))
+    participant_id = resp_json['participant']['participantId']
+    print(participant_id)
+
+    # Modify participant role
+    ## participant_id="251006506325678"
+    url = f"https://{tower_base_url}/api/orgs/{tower_org_id}/workspaces/{workspace_id}/participants/{participant_id}/role"
+    payload = { "role": "owner" }
+    resp = httpx.put(url=url, json=payload, headers=headers)
+    print(resp.status_code)
+
+    # Create Credentials
+    # Credential B64 was created by pasting the json object into a file, then
+    # piping the file through base64 on the Linux CLI:
+    # > cat key.json | base64 | tr -d \\n
+    # This means that when the credential is decoded, it is a byte string, NOT 
+    # json object. Convert the byte string to a dict, then dump the dict as
+    # stringified json (so I will be accepted by the Tower API). 
+    url = f"https://{tower_base_url}/api/credentials?workspaceId={workspace_id}"
+    credential = base64.b64decode(args.b64credential)
+    credential = json.loads(credential)
+
+    print(credential)
+    print(type(credential))
+    payload = {
+        "credentials": {
+            "name": "GoogleTest",
+            "provider": "google",
+            "keys": {
+                # "data": json.dumps(google_json_file)  # Must be string
+                "data": json.dumps(credential)
+            }
+        }
+    }
+    resp = httpx.post(url=url, json=payload, headers=headers)
+    print(resp.content)
---
+++ test_tower_label_limit2.py
@@ -0,0 +1,82 @@
+import httpx
+import argparse
+
+parser  = argparse.ArgumentParser(description='Test Nextflow Tower Label limit (default 100)')
+parser.add_argument("--action", help="Specify `create` or `delete`")
+parser.add_argument("--token", help="Tower PAT token")
+
+# tower_token = "YOUR_TOWER_TOKEN"
+tower_base_url = "dc.seqera.grahamwright.net"
+tower_workspace_id = "66352837741840"
+
+# Some comment1
+
+# JSON Payload to create a label.
+create_label_data = {
+  "name": "",
+  # "value": "test",
+  "resource": False
+}
+
+
+if __name__ == '__main__':
+
+    '''
+    Testing if a Tower Instance's workspace label limit has increased:
+        - By default, each workspace is allowed to have 100 labels.
+        - This value can be increased via the tower.yml value.
+        - As of Jan 6/23, the tw cli does not offer a method to interact with labels.
+    
+    USAGE:
+        1. Create a throwaway workspace to use for testing purposes.
+        2. Get the workspaceId via the Tower UI: (Workspaces tab of the Org, use the value tied to 'Id')
+        3. Update the tower variables at the top of this script.
+        4. Run `python3 test_tower_label_limit.py --action create` 
+            - If you do not see an error, your custom configuration should be assumed to be in effect.
+            - If you see an error, your custom configuration is not in effect.
+        5. Run `python3 test_tower_label_limits.py --action delete` to destroy any labels in the workspace.
+
+    CONSTRAINTS:
+        - I used a hardcoded `max=500` config value when retrieving workspace labels. If you decide to conduct testing which involves
+          more than 500 labels, you will need to modify this value too.
+    '''
+
+    args = parser.parse_args()
+
+    headers={
+        "Authorization": f"Bearer {args.token}",
+        "Accept_Version": "1.0",
+        "Content_Type": "application/json"
+    }
+
+    if args.action == 'create':
+        # Set the Create Label API target
+        url = f"https://{tower_base_url}/api/labels?workspaceId={tower_workspace_id}"
+
+        # Create labels
+        for i in range (105):
+            create_label_data['name'] = f"test-{i}"
+            resp = httpx.post(url=url, json=create_label_data, headers=headers)
+            resp = resp.json()
+            print(resp)
+
+            if 'message' in resp.keys():
+                if resp['message'] == 'Limit of 100 labels reached':
+                    print('[ERROR] - Your custom >100 workspace label limit is not in effect.')
+                    exit()
+
+
+    if args.action == 'delete':
+        # Set the Get Label API target
+        url = f"https://{tower_base_url}/api/labels?workspaceId={tower_workspace_id}&max=500&type=labels"
+
+        resp = httpx.get(url=url, headers=headers)
+        resp = resp.json()
+        print(resp)\
+
+        for label in resp['labels']:
+            label_id = label['id']
+            url = f"https://{tower_base_url}/api/labels/{label_id}?workspaceId={tower_workspace_id}"
+            resp = httpx.delete(url=url, headers=headers)
+            # resp = resp.json()  # Return payload is No Content
+            print(f"Deleting label {label_id} - {resp}")
