be7a8691a4a0476345fc8c8107d588e45c89a245
Graham Wright <gwright99@hotmail.com>
2023-01-15 00:16:22 +0000

    Added logic to python file and updated drone file

--- create_workspace.py
+++ create_workspace.py
@@ -44,39 +44,39 @@ if __name__ == '__main__':
     }
 
     # Add User to Org
-    # url = f"https://{tower_base_url}/api/orgs/{tower_org_id}/members/add"
-    # payload = { "user": args.useremail }
-    # resp = httpx.put(url=url, json=payload, headers=headers)
-    # print(resp)
+    url = f"https://{tower_base_url}/api/orgs/{tower_org_id}/members/add"
+    payload = { "user": args.useremail }
+    resp = httpx.put(url=url, json=payload, headers=headers)
+    print(resp)
 
     # Create Workspace
-    # url = f"https://{tower_base_url}/api/orgs/{tower_org_id}/workspaces"
-    # payload = create_org_data(wsname=args.wsname, useremail=args.useremail)
-    # resp = httpx.post(url=url, json=payload, headers=headers)
-    # print(resp.content)
-    # # Response comes back as b'{json}'. Convert to json to grab keys.
-    # resp_json = json.loads(resp.content.decode('utf-8'))
-    # workspace_id = resp_json['workspace']['id']
-    # print(workspace_id)
+    url = f"https://{tower_base_url}/api/orgs/{tower_org_id}/workspaces"
+    payload = create_org_data(wsname=args.wsname, useremail=args.useremail)
+    resp = httpx.post(url=url, json=payload, headers=headers)
+    print(resp.content)
+    # Response comes back as b'{json}'. Convert to json to grab keys.
+    resp_json = json.loads(resp.content.decode('utf-8'))
+    workspace_id = resp_json['workspace']['id']
+    print(workspace_id)
 
     # Add user to workspace
-    workspace_id="248571532740296"
-    # url = f"https://{tower_base_url}/api/orgs/{tower_org_id}/workspaces/{workspace_id}/participants/add"
-    # payload = { "userNameOrEmail": args.useremail }
-    # resp = httpx.put(url=url, json=payload, headers=headers)
-    # print(resp.content)
-    # # Response comes back as b'{json}'. Convert to json to grab keys.
-    # resp_json = json.loads(resp.content.decode('utf-8'))
-    # participant_id = resp_json['participant']['participantId']
-    # print(participant_id)
+    ## workspace_id="248571532740296"
+    url = f"https://{tower_base_url}/api/orgs/{tower_org_id}/workspaces/{workspace_id}/participants/add"
+    payload = { "userNameOrEmail": args.useremail }
+    resp = httpx.put(url=url, json=payload, headers=headers)
+    print(resp.content)
+    # Response comes back as b'{json}'. Convert to json to grab keys.
+    resp_json = json.loads(resp.content.decode('utf-8'))
+    participant_id = resp_json['participant']['participantId']
+    print(participant_id)
 
     # Modify participant role
-    participant_id="251006506325678"
-    # url = f"https://{tower_base_url}/api/orgs/{tower_org_id}/workspaces/{workspace_id}/participants/{participant_id}/role"
-    # payload = { "role": "owner" }
-    # resp = httpx.put(url=url, json=payload, headers=headers)
-    # print(resp.content)
+    ## participant_id="251006506325678"
+    url = f"https://{tower_base_url}/api/orgs/{tower_org_id}/workspaces/{workspace_id}/participants/{participant_id}/role"
+    payload = { "role": "owner" }
+    resp = httpx.put(url=url, json=payload, headers=headers)
+    print(resp.content)
     # Response comes back as b'{json}'. Convert to json to grab keys.
-    # resp_json = json.loads(resp.content.decode('utf-8'))
-    # participant_id = resp_json['participantId']['id']
-    # print(participant_id)
+    resp_json = json.loads(resp.content.decode('utf-8'))
+    participant_id = resp_json['participantId']['id']
+    print(participant_id)
